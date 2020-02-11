e1dcd735195bcd1b2c7fc4be250bbda734b935a1
root <root@tps.tps>
2020-02-11 15:08:02 +0530

    mail sender

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mail</title>
+</head>
+<body>
+	<form action="contact_me.php" method="post">
+		<input type="text" name="name" placeholder="Enter your name">
+		<input type="text" name="email" placeholder="Enter your email">
+		<input type="text" name="phone" placeholder="Enter your phone number">
+		<textarea name="message" type="text"></textarea>
+		<button type="submit" value="send"></button>
+	</form>
+
+</body>
+</html>
